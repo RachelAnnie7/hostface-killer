bff330d61924d48234c2b44ec977a659107c2651
Rachel Harris <rachelannie.harris@gmail.com>
2026-04-12 00:40:15 -0500

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+
+import dayjs from 'dayjs';
+
+import { navIcons, navLinks } from '#constants/index.js';
+import React from 'react'
+import useWindowStore from '#store/window.js';
+
+
+
+
+const Navbar = () => {
+
+    const { openWindow } = useWindowStore();
+
+    return <nav>
+
+        <div>
+
+            <img src="/images/logo.svg" alt="logo" />
+            <p className='font-bold'>Sagar's Portfolio</p>
+
+            <ul>
+                {navLinks.map(({ id, name, type }) => (
+                    <li key={id} onClick={() => openWindow(type)}>
+                        <p>
+                            {name}
+                        </p>
+                    </li>
+                ))}
+            </ul>
+
+        </div>
+
+
+
+        <div>
+
+            <ul>
+                {navIcons.map(({ id, img }) => (
+                    <li key={id}>
+                        <img src={img} className="icon-hover" alt={`icon-${id}`} />
+                    </li>
+                ))}
+
+            </ul>
+
+            <time datetime="">{dayjs().format("ddd MMM D h:mm A")}</time>
+
+        </div>
+    </nav>
+}
+
+export default Navbar
